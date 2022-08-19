bd040a5cd870ce6ffdbb8d70f5d7adf7ab46b785
Nelson <gmz_nlsn@yahoo.com>
2022-08-18 17:04:50 -0700

    complete

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Write your solution in this file!
+var customerName = 'bob';
+
+var setBestCustomer = function () {
+  bestCustomer = 'not bob';
+}
+const LeastFavoriteCustomer = 'low reviews';
+
+// const upperCaseCustomerName = () => customerName = customerName.toUpperCase();
+// //  console.log (upperCaseCustomerName());
+
+// const overwriteBestCustomer = () => bestCustomer = 'maybe bob'
+// // console.log(overwriteBestCustomer());
+
+// const changeLeastFavoriteCustomer = () => LeastFavoriteCustomer = 'popular'
+// // console.log(changeLeastFavoriteCustomer());
+
+
+function upperCaseCustomerName() {
+  customerName = customerName.toUpperCase()
+}
+
+function overwriteBestCustomer(){
+    bestCustomer = 'maybe bob'
+}
+function changeLeastFavoriteCustomer(){
+    LeastFavoriteCustomer = 'popular'
+}
